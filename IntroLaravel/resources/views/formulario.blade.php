81e432cd87a0ef45e2d5d0658734cf45cc29e274
AGomezUPQ <122045264@upq.edu.mx>
2024-10-28 18:41:41 -0600

    Practica 6.1. Lenguajes de interfaces y de alertas
    Se abordo el tema para cambiar el lenguaje de las alertas y de las etiquetas del lenguaje de hipertexto

--- IntroLaravel/resources/views/formulario.blade.php
+++ IntroLaravel/resources/views/formulario.blade.php
@@ -19,10 +19,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('rutaformulario') }}">Registro Clientes</a>
+                        <a class="nav-link" aria-current="page" href="{{ route('rutaformulario') }}"> {{ __('Registro Clientes') }} </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('rutaclientes') }}">Consulta Clientes</a>
+                        <a class="nav-link" href="{{ route('rutaclientes') }}"> {{ __('Consulta Clientes') }}</a>
                     </li>
                 </ul>
             </div>
@@ -53,36 +53,36 @@
   @endSession
         <div class="card font-monospace">
             <div class="card-header fs-5 text-center text-primary">
-                Registro de Clientes
+                {{ __('Registro clientes') }}
             </div>
             <div class="card-body text-justify">
                 <form action="/enviar" method="POST" >
                 @csrf()
                     <div class="mb-3">
-                        <label for="nombre" class="form-label">Nombre:</label>
+                        <label for="nombre" class="form-label">{{ __('Nombre:') }}</label>
                         <input type="text" class="form-control" name="txtnombre">
                         <small class="text-danger fst-italic"> {{ $errors->first('txtnombre') }}</small>
                     </div>
                     <div class="mb-3">
-                        <label for="apellido" class="form-label">Apellido:</label>
+                        <label for="apellido" class="form-label">{{ __('Apellido:') }}</label>
                         <input type="text" class="form-control" name="txtapellido">
                         <small class="text-danger fst-italic"> {{ $errors->first('txtapellido') }}</small>
                         <!-- La validacion del Required no siempre son muy efectivas, Required es solo una y no muy confiable -->
                         <!-- <input type="text" required class="form-control" name="txtapellido"> -->
                     </div>
                     <div class="mb-3">
-                        <label for="correo" class="form-label">Correo:</label>
+                        <label for="correo" class="form-label">{{ __('Correo:') }}</label>
                         <input type="text" class="form-control" name="txtcorreo">
                         <small class="text-danger fst-italic"> {{ $errors->first('txtcorreo') }}</small>
                     </div>
                     <div class="mb-3">
-                        <label for="telefono" class="form-label">Teléfono:</label>
+                        <label for="telefono" class="form-label">{{ __('Teléfono:') }} </label>
                         <input type="text" class="form-control" name="txttelefono">
                         <small class="text-danger fst-italic"> {{ $errors->first('txttelefono') }}</small>
                     </div>
                     <div class="card-footer text-muted">
                         <div class="d-grid gap-2 mt-2 mb-1">
-                            <button type="submit" class="btn btn-success btn-sm">Guardar Cliente</button>
+                            <button type="submit" class="btn btn-success btn-sm">{{ __('Guardar Cliente') }}</button>
                         </div>
                     </div>
                 </form>
